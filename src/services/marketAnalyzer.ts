75b3ca68ef43ba3228dd22f42846fcf4dd6a5266
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-11 00:44:44 +0530

    news api used

---
+++ src/services/marketAnalyzer.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const NEWS_API_KEY = process.env.NEWS_API_KEY; 
+
+
+export const analyzeMarketTrends = async (industry: string) => {
+    try {
+            const response=await axios.get(`https://newsapi.org/v2/everything`,{
+                params:{
+                    q:industry,
+                    apiKey:NEWS_API_KEY,
+                    sortBy:'relevancy',
+                    language:"en",
+                    pageSize:5
+                }
+            })
+          const articles=response.data.articles;
+
+          const trends = articles.map((article: any) => ({
+            title: article.title,
+            url: article.url,
+            publishedAt: article.publishedAt,
+            source: article.source.name, 
+        }));
+        return trends
+    } catch (error) {
+        console.error("Error fetching market trends:", error);
+        throw new Error("Failed to fetch market trends");
+    }
+}
