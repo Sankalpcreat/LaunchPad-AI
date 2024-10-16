76c1151e8898de6e203ec77d464ee6234ff8f37a
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-16 21:28:31 +0530

    another changes

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,25 +1,25 @@
-// src/utils/connectDB.ts
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+// // src/utils/connectDB.ts
+// import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
 
-dotenv.config();
+// dotenv.config();
 
-const MONGODB_URI = process.env.MONGO_URI;
+// const MONGODB_URI = process.env.MONGO_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable.");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("Please define the MONGODB_URI environment variable.");
+// }
 
-async function connectDB() {
-  try {
+// async function connectDB() {
+//   try {
   
-    await mongoose.connect(MONGODB_URI as string)
+//     await mongoose.connect(MONGODB_URI as string)
     
-    console.log("MongoDB connected successfully.");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    throw error; 
-  }
-}
+//     console.log("MongoDB connected successfully.");
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//     throw error; 
+//   }
+// }
 
-export default connectDB;
+// export default connectDB;
